766d1ee258399532677b7dd4597f16770f890a18
omar94khan <omar.ahmed.khan@hotmail.com>
2022-05-20 01:57:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-   
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+from sklearn.preprocessing import MinMaxScaler
 
 def main():
     st.title('Fraud Detector')
@@ -13,13 +13,15 @@ def main():
         classifier = pickle.load(open('RandomForrestClassifier_df4x.pkl', 'rb'))
         
         df = pd.read_csv(file)
-        for col in ['Class']:
-            df = df.loc[:,df.columns != col]
-        df = pd.DataFrame(MinMaxScaler().fit(df).transform(df), columns=df.columns)
         st.write("The dataset you uploaded is:")
         st.write(df)
 
-        result_df = pd.DataFrame(classifier.predict_proba(df))
+        for col in ['Class']:
+            df = df.loc[:,df.columns != col]
+        df = pd.DataFrame(MinMaxScaler().fit(df).transform(df), columns=df.columns)
+
+
+        result_df = pd.DataFrame(classifier.predict_proba(df)[1])
             
         st.write("Output DataFrame depicting probability of the transaction being fraudulant.")
         st.write(result_df)
